ba53b76cf776da0c3626db70744debd1f31dbdb8
aliaksandr-naumionak <aliaksandr.naumionak@ventionteams.com>
2025-08-01 10:26:58 +0300

    rate limiting

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
+import * as dotenv from 'dotenv';
 import Fastify from 'fastify';
+import FastifyRateLimit from '@fastify/rate-limit';
 import urlRoutes from './routes/url.routes';
 import prismaPlugin from './plugins/prisma';
-import * as dotenv from 'dotenv';
+import { config } from './utils/config';
 
 dotenv.config();
 
 const server = Fastify({ logger: true });
+
+server.register(FastifyRateLimit, {
+    max: config.RATE_LIMIT_MAX, // max requests
+    timeWindow: config.RATE_LIMIT_WINDOW, // per time window
+    errorResponseBuilder: () => ({
+        statusCode: 429,
+        error: 'Too Many Requests',
+        message: 'You have exceeded the request limit.',
+    }),
+});
 
 server.register(prismaPlugin);
 server.register(urlRoutes, { prefix: '/api' });
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,4 +3,6 @@ export const config = {
     BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
     DEFAULT_PAGE: parseInt(process.env.DEFAULT_PAGE || '1', 10),
     DEFAULT_LIMIT: parseInt(process.env.DEFAULT_LIMIT || '10', 10),
+    RATE_LIMIT_MAX: parseInt(process.env.RATE_LIMIT_MAX || '100', 10),
+    RATE_LIMIT_WINDOW: process.env.RATE_LIMIT_WINDOW || '1 minute',
 };
